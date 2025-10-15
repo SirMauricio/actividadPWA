e9cdb2f0083b1e6b42904d7441e98605ba0d4779
SirMauricio <190300383@unicaribe.edu.mx>
2025-10-14 19:11:50 -0500

    Versión inicial de mi To-Do PWA

--- src/Splash.js
+++ src/Splash.js
@@ -1,15 +1,14 @@
 import React, { useEffect, useState } from "react";
 import "./Splash.css";
-import logo from "./icons/icon-192x192.png"; 
 
 function Splash({ onFinish }) {
 const [visible, setVisible] = useState(true);
 
-    useEffect(() => {
+useEffect(() => {
     const timer = setTimeout(() => {
-        setVisible(false);
-      if (onFinish) onFinish(); // Llamamos onFinish solo si existe
-    }, 2000); // 2 segundos
+    setVisible(false);
+    if (onFinish) onFinish();
+    }, 2000);
     return () => clearTimeout(timer);
 }, [onFinish]);
 
@@ -17,10 +16,10 @@ if (!visible) return null;
 
 return (
     <div className="splash-screen">
-    <img src="/icons/icon-192x192.png" alt="Logo" className="splash-logo" />
-    <h1>TlabajApp</h1>
+        <img src="/icons/icon-192x192.png" alt="Logo" className="splash-logo" />
+        <h1>TlabajApp</h1>
     </div>
-);
+    );
 }
 
 export default Splash;
